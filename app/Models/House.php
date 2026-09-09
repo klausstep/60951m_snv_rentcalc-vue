6dd9f25e7f98baf39b71666b5045621092c17672
klausstep <klausstepmailbox@gmail.com>
2026-09-09 16:37:48 +0500

    обновлён favicon и модель House

--- app/Models/House.php
+++ app/Models/House.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class House extends Model
 {
-    protected $fillable = ['address', 'name'];
+    protected $fillable = ['address', 'name', 'rate'];
 
     public function flats()
     {
